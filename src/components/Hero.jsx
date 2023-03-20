6004fcb4ee72407a2b0ebc203cb5979478dc7d5c
Xun Zhu <zhu.xun1@northeastern.edu>
2023-03-20 01:31:19 -0700

    finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { Profile } from "./canvas";
 
 const Hero = () => {
   return (
@@ -25,7 +26,9 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/*<ComputersCanvas />*/}
+      {/*<img src="../assets/logo.jpeg" alt="logo" />*/}
+      <Profile />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
---
+++ src/components/canvas/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="float-right">
+      <img
+        className="object-cover h-[500px] w-[500px] mt-16 mr-16"
+        src="public/head.jpeg"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Profile;
